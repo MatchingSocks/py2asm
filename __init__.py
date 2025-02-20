08192c6f63285a33c777ac3d50998e9b060a6a54
JT Wells <jtquick@gmail.com>
2025-02-20 06:50:56 -0600

    Added conftest.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Add the src directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
-
-# Add the tests directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "tests")))
